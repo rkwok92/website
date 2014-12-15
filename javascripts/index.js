3423e6a98b45c721e0547e53536a076b23a3f2f7
Ryan Kwok <rkwok@andrew.cmu.edu>
2014-12-14 16:55:36 -0800

    temp index added

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,53 +1,15 @@
-$(document).ready(function() {
-	$(function(){
-		$('#header_container').hide();
-		$('#mainpage').hide();
-		$('#header_container').fadeIn(800);
-		$('#mainpage').delay(500).fadeIn(1200);
-	});
-
-	$("#work").hover(function(){
-	 $("#work").css("color", "#F8A71A");
-	}, function(){
-	 $("#work").css("color", "#FFFFFF");
-	});
-	$("#play").hover(function(){
-	 $("#play").css("color", "#F8A71A");
-	}, function(){
-	 $("#play").css("color", "#FFFFFF");
-	});
-	$("#contact").hover(function(){
-	 $("#contact").css("color", "#F8A71A");
-	}, function(){
-	 $("#contact").css("color", "#FFFFFF");
-	});
-
- //localstorage on pagevisits
- function pageVisit()
- {
-	 console.log(localStorage.visitCount);
-   if (localStorage.visitCount){
-     localStorage.visitCount=Number(localStorage.visitCount)+1;
-     }
-   else{
-     localStorage.visitCount=1;
-     }
-   if (localStorage.visitCount>50){
-     $("#visitNumber").html("you've stumbled onto my site " + localStorage.visitCount + " times now. . .   I'm filing a restraining order.");
-     }
-   else if (localStorage.visitCount>20){
-     $("#visitNumber").html("you've stumbled onto my site " + localStorage.visitCount + " times now. . .   are you stalking me?");
-     }
-   else if (localStorage.visitCount>5){
-     $("#visitNumber").html("you've stumbled onto my site " + localStorage.visitCount + " times now. I'm flattered! ");
-     }
-	 else if (localStorage.visitCount>1){
-	 	 $("#visitNumber").html("you've stumbled onto my site " + localStorage.visitCount + " times now. Thought you'd like to know.");
-	   }
-   else{
-     $("#visitNumber").html("I noticed you've never visited my site before, thanks for dropping by!");
-     }
-  };
-	window.onload = pageVisit;
-
+$(function(){
+	
+	$('#mainpage').hide();
+	$('#mainpage').delay(500).fadeIn(1200);
+	 
+	 var array = ["remodeling the kitchen right now", "terrible breakout right now", "rearranging some furniture right now", 
+		 "redoing the lights right now", "adding some decor right now", "running low on sleep right now", 
+		 "doing some renovation right now", "fixing the walls right now", "remaking a floorplan right now", 
+		 "door needs repairs right now", "still waiting on the guys from legal and risk", "working on my jumpshot right now"];
+	 var number = Math.ceil(Math.random()*array.length-1);
+	 $("#random").html(array[number]);
+	 
 });
+
+
