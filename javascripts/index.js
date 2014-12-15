5fb1e76e8b1ab61b41ddaada724d62471eedff78
rkwok92 <rkwok92@gmail.com>
2014-12-14 18:27:12 -0800

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,10 +3,9 @@ $(function(){
 	$('#mainpage').hide();
 	$('#mainpage').delay(500).fadeIn(1200);
 	 
-	 var array = ["remodeling the kitchen right now", "terrible breakout right now", "rearranging some furniture right now", 
-		 "redoing the lights right now", "adding some decor right now", "running low on sleep right now", 
-		 "doing some renovation right now", "fixing the walls right now", "remaking a floorplan right now", 
-		 "door needs repairs right now", "still waiting on the guys from legal and risk", "working on my jumpshot right now"];
+	 var array = ["still remodeling the kitchen", "terrible breakout - you don't want to see me", "still rearranging the furniture", 
+		 "still redoing the lights", "running low on sleep", "still getting dressed", "rain check",
+		 "doorman's taking a couple days off", "still waiting on the guys from legal and risk", "working on my jumpshot right now"];
 	 var number = Math.ceil(Math.random()*array.length-1);
 	 $("#random").html(array[number]);
 	 
