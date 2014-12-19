217af43475c200ce02bc7baa9c5bb8120b893d90
rkwok92 <rkwok92@gmail.com>
2014-12-18 22:25:18 -0800

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -4,9 +4,9 @@ $(function(){
 	$('#mainpage').delay(500).fadeIn(1200);
 	 
 	 var array = ["still remodeling the kitchen", "terrible breakout - you don't want to see me", "still rearranging the furniture", 
-		 "still waiting on the guys from legal and risk", "still redoing the lights", "running low on sleep", "still getting dressed",
-		 "doorman's taking a couple days off", "still waiting on the guys from legal and risk", "working on my jumpshot right now",
-		 "behind on my utilities bill", "still renovating the place",  "rain check"];
+		 "still waiting on the guys from legal and risk", "still redoing the lights", "running low on sleep",
+		 "doorman's on leave", "still waiting on the guys from legal and risk", "working on my jumpshot right now",
+		 "still renovating the place"];
 	 var number = Math.ceil(Math.random()*array.length-1);
 	 $("#random").html(array[number]);
 	 
